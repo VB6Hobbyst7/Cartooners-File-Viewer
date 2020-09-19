9ba360b6215aa96446066ba1f29a977a8561de93
Peter Swinkels <peter_swinkels@hotmail.com>
2020-09-19 15:50:15 +0200

    Add files via upload

---
+++ Binary Data Modifier/Core.vb
@@ -0,0 +1,127 @@
+﻿'This module's imports and settings.
+Option Compare Binary
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Environment
+Imports System.Convert
+Imports System.Diagnostics
+Imports System.IO
+Imports System.Linq
+
+'This module contains this program's core procedures.
+Public Module CoreModule
+
+   'This procedure is executed when this program is started.
+   Public Sub Main()
+      Try
+         Dim Arguments As String = Nothing
+         Dim Data As List(Of Byte) = Nothing
+         Dim InputPath As String = Nothing
+         Dim Length As Integer = Nothing
+         Dim ModificationType As Char = Nothing
+         Dim NewData As List(Of Byte) = Nothing
+         Dim Offset As Integer = Nothing
+         Dim OpenWith As String = Nothing
+         Dim OutputPath As String = Nothing
+         Dim ScriptPath As String = GetCommandLineArgs.Last()
+
+         Using ScriptFile As New StreamReader(ScriptPath)
+            With ScriptFile
+               InputPath = .ReadLine()
+               OutputPath = .ReadLine()
+               Offset = Integer.Parse(.ReadLine())
+               Length = Integer.Parse(.ReadLine())
+               ModificationType = .ReadLine().Trim().ToUpper().ToCharArray().First()
+               If ModificationType = "R"c Then NewData = New List(Of Byte)(From [Byte] In .ReadLine().Split(","c) Select Byte.Parse([Byte]))
+               If Not .EndOfStream() Then OpenWith = .ReadLine()
+               If Not .EndOfStream() Then Arguments = .ReadLine()
+            End With
+         End Using
+
+         Data = New List(Of Byte)(File.ReadAllBytes(InputPath))
+
+         Select Case ModificationType
+            Case "I"c
+               Data = InvertByteBits(Data, Offset, Length)
+            Case "R"c
+               Data = ReplaceBytes(Data, NewData, Offset, Length)
+            Case "Z"c
+               Data = ZeroBytes(Data, Offset, Length)
+            Case Else
+               Console.WriteLine($"Unknown modification type: ""{ModificationType}"".")
+         End Select
+
+         File.WriteAllBytes(OutputPath, Data.ToArray())
+
+         If OpenWith IsNot Nothing Then Process.Start(New ProcessStartInfo With {.FileName = OpenWith, .Arguments = Arguments, .WindowStyle = ProcessWindowStyle.Normal})
+      Catch ExceptionO As Exception
+         Console.WriteLine($"Error: {ExceptionO.Message}")
+      End Try
+   End Sub
+
+   'This procedure the bits of the specified number of bytes at the specified offset inside the specified data and returns the result.
+   Private Function InvertByteBits(Data As List(Of Byte), Offset As Integer, Length As Integer) As List(Of Byte)
+      Try
+         Dim Position As Integer = Offset
+
+         Do Until Position >= Offset + Length OrElse Position >= Data.Count
+            Data(Position) = ToByte(Data(Position) Xor &HFF%)
+            Position += &H1%
+         Loop
+
+         Return Data
+      Catch ExceptionO As Exception
+         Console.WriteLine($"Error: {ExceptionO.Message}")
+      End Try
+
+      Return New List(Of Byte)
+   End Function
+
+   'This procedure replaces the data of the specified length at the specified offset inside the data specified with the new data specified and returns the result.
+   Private Function ReplaceBytes(Data As List(Of Byte), NewData As List(Of Byte), Offset As Integer, Length As Integer) As List(Of Byte)
+      Try
+         Dim Position As Integer = Offset
+
+         Do Until Position + NewData.Count >= Offset + Length OrElse Position >= Data.Count
+            For SubPosition As Integer = Position To Position + (NewData.Count - &H1%)
+               If SubPosition >= Data.Count Then Exit For
+               Data(SubPosition) = NewData(SubPosition - Position)
+            Next SubPosition
+            Position += NewData.Count
+         Loop
+
+         If Position < Offset + (Length - &H1%) AndAlso Position < Data.Count Then
+            For SubPosition As Integer = Position To Offset + (Length - &H1%)
+               If SubPosition >= Data.Count Then Exit For
+               Data(SubPosition) = NewData(SubPosition - Position)
+            Next SubPosition
+         End If
+
+         Return Data
+      Catch ExceptionO As Exception
+         Console.WriteLine($"Error: {ExceptionO.Message}")
+      End Try
+
+      Return New List(Of Byte)
+   End Function
+
+   'This procedure zeroes the specified number of bytes at the specified offset inside the specified data and returns the result.
+   Private Function ZeroBytes(Data As List(Of Byte), Offset As Integer, Length As Integer) As List(Of Byte)
+      Try
+         If Offset + Length >= Data.Count Then Length = Data.Count - Offset
+
+         Data.RemoveRange(Offset, Length)
+         Data.InsertRange(Offset, Enumerable.Repeat(ToByte(&H0%), Length))
+
+         Return Data
+      Catch ExceptionO As Exception
+         Console.WriteLine($"Error: {ExceptionO.Message}")
+      End Try
+
+      Return New List(Of Byte)
+   End Function
+End Module
---
+++ Binary Data Modifier/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Modifier")>
+<Assembly: AssemblyDescription("This program is designed to assist in analyzing binary data by analyzing the result of modifying it partially or in whole.")>
+<Assembly: AssemblyCompany("Peter Swinkels")>
+<Assembly: AssemblyProduct("Modifier")>
+<Assembly: AssemblyCopyright("2020")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("539293e0-97df-47d6-9d6b-b2c614fc81be")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
